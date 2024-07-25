cb9b800c3f962af5297d50a8af600d972902512c
AndreyLoginov86 <loginovar86@gmail.com>
2024-07-25 22:16:10 +0400

    Практика 1-я. Работа с решениями и проектами

---
+++ Task1A/Task1A.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task1.CSharpBase
+{
+    public static class Task1A
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Task1A");
+            Console.Write("Нажмите <Enter> для завершения");
+            while (Console.ReadKey(true).Key != ConsoleKey.Enter) { };
+        }
+    }
+}
---
+++ Task1B/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Task1B
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Task1B");
+            Console.Write("Нажмите <Enter> для завершения");
+            while (Console.ReadKey(true).Key != ConsoleKey.Enter) { };
+        }
+    }
+}
